5e3893aa4ebb53706f5fe15bd706e740db4550c6
root <root@ubuntu.ubuntu-domain>
2013-03-24 11:58:03 +0900

    sample ruby program

---
+++ sample_rbmon.rb
@@ -0,0 +1,24 @@
+require 'mongo_mapper'
+require 'date'
+
+# hostname = mongodbIPAddress or hostname
+# mongodb_port = mongodbPort
+# mongodbconnect
+MongoMapper.connection = Mongo::Connection.from_uri("mongodb://172.16.215.129:27017")
+# DBconnect
+MongoMapper.database = 'ruby_mongo'
+
+# make class
+class AutoUserCreate
+	include MongoMapper::Document
+	key :name,String
+	key :user_id,Integer
+	key :_id,Integer
+end
+
+# Main
+# DB ADD CORECTION AND USERS
+(1..200000).each do |i|
+	date = Time.now.to_i
+	AutoUserCreate.create(:_id=>date,:user_id => i,:name => "rusla_#{i}")
+end
